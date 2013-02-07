941e12fb95c9b69692c88b1485470e7c1eb0f3fc
Matthew Michaud <michaudm3@wit.edu>
2013-02-07 16:02:47 -0500

    Fixed snapping cursor bug. Added Set Ip right click option

--- src/com/bengreenier/blackhole/core/UserBlackhole.java
+++ src/com/bengreenier/blackhole/core/UserBlackhole.java
@@ -2,6 +2,8 @@ package com.bengreenier.blackhole.core;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Robot;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.dnd.DnDConstants;
@@ -33,6 +35,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 
 import com.bengreenier.blackhole.util.ByteArray;
@@ -65,6 +68,11 @@ public class UserBlackhole {
 	private Lock lock;
 	private JFrame frame;
 	private DropTarget dropTarget;
+	
+	int mouse_click_X = 0;
+	int mouse_click_Y = 0;
+	
+	private String ip_address = "127.0.0.1";
 
 	public UserBlackhole(String[] args) {
 		this.args = args;
@@ -93,36 +101,28 @@ public class UserBlackhole {
 		//askWindowExit();
 	}
 
-
 	public UserBlackhole start() {
 		frame.setIconImage(new ImageIcon("res/drawable/blackhole.png").getImage());
 		frame.setTitle("Blackhole");
 		frame.setUndecorated(true);
 		frame.setBackground(new Color(0,0,0,0));
-		frame.setBounds(Integer.parseInt(prop.getProperty("location-x")), Integer.parseInt(prop.getProperty("location-y")), 183, 179);
+		frame.setBounds(Integer.parseInt(prop.getProperty("location-x")), Integer.parseInt(prop.getProperty("location-y")), 128, 128);
 		frame.setVisible(true);
-
+		ip_address = prop.getProperty("ip_address");
 		
 		dropTarget = new DropTarget(frame,new DropTargetListener(){
-
 			@Override
 			public void dragEnter(DropTargetDragEvent arg0) {
 				// TODO Auto-generated method stub
-
 			}
-
 			@Override
 			public void dragExit(DropTargetEvent arg0) {
 				// TODO Auto-generated method stub
-
 			}
-
 			@Override
 			public void dragOver(DropTargetDragEvent arg0) {
 				// TODO Auto-generated method stub
-
 			}
-
 			@SuppressWarnings("unchecked")
 			@Override
 			public void drop(DropTargetDropEvent dtde) {
@@ -136,15 +136,12 @@ public class UserBlackhole {
 						if (flavors[i].isFlavorJavaFileListType()) {
 							dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
 							System.out.println("Good Drop!");
-
 							// And add the list of file names to our text area
 							java.util.List<Object> list = (java.util.List<Object>)tr.getTransferData(flavors[i]);
-
 							filesDropped(list);
 						}
 					}
 				}catch (Exception e) {
-
 				}
 			}
 
@@ -163,21 +160,19 @@ public class UserBlackhole {
 
 			}
 		});
+		
 		frame.addMouseMotionListener(new MouseMotionListener(){
-
+			
 			@Override
 			public void mouseDragged(MouseEvent arg0) {
-
 				if (arg0.getButton() == 0) {
-					//TODO make this not "jump" to top left to mouse
-					int oX = (int)(frame.getLocation().getX()+arg0.getX());
-					int oY = (int)(frame.getLocation().getY()+arg0.getY());
-
-
+					int oX = (int)(arg0.getXOnScreen()-mouse_click_X);
+					int oY = (int)(arg0.getYOnScreen()-mouse_click_Y);
+					
 					frame.setLocation(oX, oY);
 				}
 			}
-
+			
 			@Override
 			public void mouseMoved(MouseEvent arg0) {
 				// TODO Auto-generated method stub
@@ -185,94 +180,50 @@ public class UserBlackhole {
 			}});
 
 		JLabel label = new JLabel(new ImageIcon("res/drawable/blackhole.png"));
-		label.setBounds(0, 0,183,179);
-
-		JLabel xout = new JLabel("x");
-		xout.addMouseListener(new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				frame.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				frame.setCursor(Cursor.getDefaultCursor());
-
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				askWindowExit();
-
-			}});
-		xout.setBounds(173, 0, 10, 10);
-
-
+		label.setBounds(0, 0,128,128);
+		
 		final JPopupMenu rightClickMenu = new JPopupMenu();
 		JMenuItem rightClickExit = new JMenuItem("Exit");
 		rightClickExit.addActionListener(new ActionListener(){
-
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				askWindowExit();
-
 			}});
+		JMenuItem rightClickIP = new JMenuItem("Set IP");
+		rightClickIP.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				askSetIP();
+			}});
+		rightClickMenu.add(rightClickIP);
 		rightClickMenu.add(rightClickExit);
-
-
-
 
 		frame.addMouseListener(new MouseListener(){
 
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
+			public void mouseClicked(MouseEvent arg0) {}
 
 			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
+			public void mouseEntered(MouseEvent arg0) {}
 
 			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
+			public void mouseExited(MouseEvent arg0) {}
 
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
+				mouse_click_X = arg0.getX();
+				mouse_click_Y = arg0.getY();
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				if (arg0.getButton() == MouseEvent.BUTTON3)
 					rightClickMenu.show(frame, arg0.getX(),arg0.getY());
-
+					
 			}});
 
-
 		frame.add(label);
-		frame.add(xout);
-
+		
 		frame.repaint();
 		return this;
 	}
@@ -303,11 +254,24 @@ public class UserBlackhole {
 		//this results in the window calling exit()
 		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
+	
+	private void askSetIP() {
+		//this results is the window popping up a new input dialog to change the IP address
+		ip_address = (String)JOptionPane.showInputDialog(
+		                    frame,
+		                    "Enter in new IP address",
+		                    "Set IP",
+		                    JOptionPane.PLAIN_MESSAGE,
+		                    null,
+		                    null,
+		                    ip_address);
+	}
 
 	private void writePropDefaults() {
 		prop.setProperty("save-location", "true");
 		prop.setProperty("location-x", "100");
 		prop.setProperty("location-y", "100");
+		prop.setProperty("ip_address", "127.0.0.1");
 	}
 
 	private void writePropExit() {
@@ -315,11 +279,13 @@ public class UserBlackhole {
 			prop.setProperty("location-x", ""+(int)frame.getLocation().getX());
 			prop.setProperty("location-y", ""+(int)frame.getLocation().getY());
 		}
+		if(ip_address != prop.getProperty("ip_address")) {
+			prop.setProperty("ip_address", ip_address);
+		}
 	}
 
 	private void filesDropped(final java.util.List<Object> list) {
 		//TODO prompt for server information  
-		final String serverIP = "127.0.0.1";
 		final int port = Port.DEFAULT;
 
 		//open a thread to send the files.
@@ -329,7 +295,7 @@ public class UserBlackhole {
 
 				Socket socket = new Socket();
 				try {
-					socket.connect(new InetSocketAddress(serverIP,port));
+					socket.connect(new InetSocketAddress(ip_address,port));
 				} catch (NumberFormatException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
