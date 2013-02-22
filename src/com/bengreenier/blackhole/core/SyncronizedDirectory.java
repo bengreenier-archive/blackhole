b0140bd8e148861cd9c5991dc801e1c1d6c21704
B3ngr33ni3r <mygithub@b3nhub.com>
2013-02-22 16:24:48 -0500

    recursive syncronizeddirectory now

--- src/com/bengreenier/blackhole/core/SyncronizedDirectory.java
+++ src/com/bengreenier/blackhole/core/SyncronizedDirectory.java
@@ -8,25 +8,37 @@ import java.util.TimerTask;
 import com.bengreenier.blackhole.util.FileIO;
 import com.bengreenier.blackhole.util.MD5;
 
+
 public class SyncronizedDirectory {
-	public interface SyncInterface {
-		public void createFile(File f);
-		public void updateFile(File f);
-		public void removeFile(File f);
+	public static class SyncronizedDirectoryException extends Exception {
+		/**
+		 * suid, so this can be serialized properly if needed
+		 */
+		private static final long serialVersionUID = 6187747069322362175L;
+
+		public SyncronizedDirectoryException(String message) {
+			super(message);
+		}
 	}
-	
-	
+
+	public static interface SyncInterface {
+		public void createFile(File f) throws SyncronizedDirectoryException;
+		public void updateFile(File f) throws SyncronizedDirectoryException;
+		public void removeFile(File f) throws SyncronizedDirectoryException;
+	}
+
+
 	private Timer syncer;//the scheduler timer for the process
 	private long delay,wait;//the args for the timer
 	private File root; //the root directory to sync
 	private HashMap<File,String> hashes;//store the file hashes here
 	private SyncInterface si;//the interface where we make calls to mod the file stuct
-	
-	public SyncronizedDirectory(long delay,String directory, SyncInterface si) throws Exception {
+
+	public SyncronizedDirectory(long delay,String directory, SyncInterface si) throws SyncronizedDirectoryException {
 		if (!new File(directory).exists())
-			throw new Exception("Invalid SyncronizedDirectory: directory doesn't exist.");
+			throw new SyncronizedDirectoryException("Invalid SyncronizedDirectory: directory doesn't exist.");
 		if (!new File(directory).isDirectory())
-			throw new Exception("Invalid SyncronizedDirectory: invalid directory.");
+			throw new SyncronizedDirectoryException("Invalid SyncronizedDirectory: invalid directory.");
 		this.si = si;
 		this.root = new File(directory);
 		this.hashes = new HashMap<File,String>();
@@ -34,39 +46,91 @@ public class SyncronizedDirectory {
 		this.wait = 0;
 		syncer = new Timer();
 	}
-	
+
+	//call this to begin execution
 	public void start() {
 		syncer.scheduleAtFixedRate(new TimerTask(){
 
+			private HashMap<File,String> temp;
+			
+			private void recurse(File dir) {
+				for (File f : dir.listFiles()) {
+					if (f.isDirectory()) {
+						recurse(f);
+					} else {
+						if (!hashes.containsKey(f)) {
+							hashes.put(f, new MD5(FileIO.getByteArray(f.getPath())).getStringDigest());//eww
+							try {
+								si.createFile(f);
+							} catch (SyncronizedDirectoryException e) {
+								e.printStackTrace();
+							}
+						} else {
+							String td = new MD5(FileIO.getByteArray(f.getPath())).getStringDigest();//temp digest
+							if (!hashes.get(f).equals(td)) {
+								//if file has changed
+								hashes.remove(f);
+								hashes.put(f, td);
+								try {
+									si.updateFile(f);
+								} catch (SyncronizedDirectoryException e) {
+									e.printStackTrace();
+								}
+							}
+						}
+						temp.remove(f);
+					}
+				}
+			}
+			
 			@Override
 			public void run() {
-				HashMap<File,String> temp = new HashMap<File,String>(hashes);
-				
-				
-				for (File f : root.listFiles()) {
-					if (!hashes.containsKey(f)) {
-						hashes.put(f, new MD5(FileIO.getByteArray(f.getName())).getStringDigest());//eww
-						si.createFile(f);
-					} else {
-						String td = new MD5(FileIO.getByteArray(f.getName())).getStringDigest();//temp digest
-						if (!hashes.get(f).equals(td)) {
-							//if file has changed
-							hashes.remove(f);
-							hashes.put(f, td);
-							si.updateFile(f);
-						}
-					}
-					temp.remove(f);
-				}
+				temp = new HashMap<File,String>(hashes);
+
+				recurse(root);
 				
 				if (temp.size()>0) {
 					for (File f : temp.keySet())
-						si.removeFile(f);
+						try {
+							si.removeFile(f);
+						} catch (SyncronizedDirectoryException e) {
+							e.printStackTrace();
+						}
 				}
-					
+				temp.clear();//just to be sure. even though we WILL recreate it
+
 			}
-			
+
 		}, wait, delay);
 	}
-	
+
+	public void stop() {
+		syncer.cancel();
+	}
+
+
+	/**
+	 * test case, shows example usage of this class.
+	 * @param args
+	 * @throws SyncronizedDirectoryException 
+	 */
+	public static void main(String[] args) throws SyncronizedDirectoryException {
+		SyncronizedDirectory sd = new SyncronizedDirectory(1000, ".", new SyncInterface(){
+
+			@Override
+			public void createFile(File f) throws SyncronizedDirectoryException {
+				System.out.println("this method would create "+f.getPath()+" in the file system.");
+			}
+
+			@Override
+			public void updateFile(File f) throws SyncronizedDirectoryException {
+				System.out.println("this method would update, or possibly remove and create "+f.getPath()+" in the file system.");
+			}
+
+			@Override
+			public void removeFile(File f) throws SyncronizedDirectoryException {
+				System.out.println("this method would remove "+f.getPath()+" from the file system.");
+			}});
+		sd.start();
+	}
 }
